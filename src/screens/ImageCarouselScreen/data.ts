dbe7da590290539cbfc11ed493616a2cbfd051b2
danivanov443 <danivanov443@gmail.com>
2023-12-15 17:54:41 +0300

    added initial image carousel

---
+++ src/screens/ImageCarouselScreen/data.ts
@@ -0,0 +1,7 @@
+export const data: string[] = [
+  'https://aambassador.com/images/car/car-1-old.jpg',
+  'https://www.rybrook.co.uk/mclaren/wp-content/uploads/sites/6/2023/09/McL_23_08_11_03-750S-Spider-640x360.jpg',
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Aston_martin_vantage_safety_car_f1_2021.jpg/640px-Aston_martin_vantage_safety_car_f1_2021.jpg',
+  'https://seoul.japantransit.ru/4734078/conversions/35109634_001-thumb.jpg',
+  'https://cdn.iz.ru/sites/default/files/styles/640x360/public/news-2023-11/KK301864.JPG.jpg?itok=Ke_N6Pk4',
+];
--- src/screens/ImageCarouselScreen/index.tsx
+++ src/screens/ImageCarouselScreen/index.tsx
@@ -1,13 +1,110 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {Image, StyleSheet, View, useWindowDimensions} from 'react-native';
+import {data} from './data';
+import Animated, {
+  useSharedValue,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  interpolate,
+  useAnimatedRef,
+} from 'react-native-reanimated';
+
+type SpacerItem = {key: 'spacer-left' | 'spacer-right'};
 
 function ImageCarouselScreen(): React.JSX.Element {
-  const backgroundStyle = {
-    backgroundColor: 'white',
-    flex: 1,
-  };
+  const scrollViewRef = useAnimatedRef<Animated.ScrollView>();
+  const interval = useRef<NodeJS.Timeout>();
+  const initialData: (string | SpacerItem)[] = [
+    {key: 'spacer-left'},
+    ...data,
+    {key: 'spacer-right'},
+  ];
+  const [newData] = useState(initialData);
+  const [isAutoPlay, setIsAutoPlay] = useState(true);
+  const {width} = useWindowDimensions();
+  const SIZE = width * 0.8;
+  const SPACER = (width - SIZE) / 2;
+  const x = useSharedValue(0);
+  const offSet = useSharedValue(0);
 
-  return <View style={backgroundStyle}></View>;
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: event => {
+      x.value = event.contentOffset.x;
+    },
+  });
+
+  useEffect(() => {
+    if (isAutoPlay) {
+      let _offSet = offSet.value;
+      interval.current = setInterval(() => {
+        if (_offSet >= Math.floor(SIZE * (data.length - 1) - 10)) {
+          _offSet = 0;
+        } else {
+          _offSet = Math.floor(_offSet + SIZE);
+        }
+        scrollViewRef.current?.scrollTo({x: _offSet, y: 0});
+      }, 2000);
+    } else {
+      clearInterval(interval.current);
+    }
+  }, [SIZE, SPACER, isAutoPlay, offSet.value, scrollViewRef]);
+  return (
+    <View>
+      <Animated.ScrollView
+        ref={scrollViewRef}
+        onScroll={onScroll}
+        onScrollBeginDrag={() => {
+          setIsAutoPlay(false);
+        }}
+        onMomentumScrollEnd={e => {
+          offSet.value = e.nativeEvent.contentOffset.x;
+          setIsAutoPlay(true);
+        }}
+        scrollEventThrottle={16}
+        decelerationRate="fast"
+        snapToInterval={SIZE}
+        horizontal
+        bounces={false}
+        showsHorizontalScrollIndicator={false}>
+        {newData.map((item, index) => {
+          // eslint-disable-next-line react-hooks/rules-of-hooks
+          const style = useAnimatedStyle(() => {
+            const scale = interpolate(
+              x.value,
+              [(index - 2) * SIZE, (index - 1) * SIZE, index * SIZE],
+              [0.88, 1, 0.88],
+            );
+            return {
+              transform: [{scale}],
+            };
+          });
+          if (!item) {
+            return <View style={{width: SPACER}} key={index} />;
+          }
+          return (
+            <View style={{width: SIZE}} key={index}>
+              <Animated.View style={[styles.imageContainer, style]}>
+                <Image source={{uri: item as string}} style={styles.image} />
+              </Animated.View>
+            </View>
+          );
+        })}
+      </Animated.ScrollView>
+    </View>
+  );
 }
 
 export default ImageCarouselScreen;
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    borderRadius: 18,
+    overflow: 'hidden',
+    backgroundColor: 'pink',
+  },
+  image: {
+    width: '100%',
+    height: undefined,
+    aspectRatio: 16 / 9,
+  },
+});
